3e737f12d0952e06c0192071c168e3ce9e797f8d
Tianri Gu <tianrigu@Rmac.local>
2017-06-08 00:38:50 -0700

    change file name

--- dht/dht_general.go
+++ dht/dht_general.go
@@ -1,9 +1,38 @@
 package dht
 
+/*
+	R-net can apply different DHTs as required to optimize the overall performance
+	This file defines the general interfaces for all DHT and the dispatcher. 
+	
+	INIT
+	JOIN
+	REPLICATE
+	CHECK_PEER
+	NOTIFY
+	SEND
+	.
+	.
+	.
+	
+*/
+
 import (
 	"fmt"
 )
 
-func Echo(msg string) {
-	fmt.Println(msg)
+const DHT_PREFIX string = "[DHT]\t"
+
+type Node struct {
+	IP string
+	Port_int int
+	Port_string string
+	ID string
+}
+
+// First time initialization
+func Self_init(ip string, port int, topo string) *Node {
+	fmt.Printf("%sMy IP is [%s:%d] using [%s] as base arch\n", DHT_PREFIX, ip, port, topo)
+	new_node := &Node{ip, port, "", ""}
+
+	return new_node
 }
